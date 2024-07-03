c7b11387713ad18d787e18c82ebf23a0ad4d7b50
MauroDegaspari <mauro_degaspari@hotmail.com>
2024-07-03 11:45:26 -0300

    ajuste em Exceptions

--- HotelariaDegaspari/src/main/java/br/com/HotelariaDegaspari/infrastructure/exceptions/handle/GlobalExceptionHandle.java
+++ HotelariaDegaspari/src/main/java/br/com/HotelariaDegaspari/infrastructure/exceptions/handle/GlobalExceptionHandle.java
@@ -23,12 +23,12 @@ public class GlobalExceptionHandle {
 	
 	@ExceptionHandler(ConflitoException.class)
 	public ResponseEntity<ErrosResponse> handleResponseEntity(ConflitoException cf, HttpServletRequest request){		
-		return response(cf.getMessage(), request, HttpStatus.INTERNAL_SERVER_ERROR, LocalDateTime.now());		
+		return response(cf.getMessage(), request, HttpStatus.CONFLICT, LocalDateTime.now());		
 	}
 	
 	@ExceptionHandler(ImprocessavelException.class)
 	public ResponseEntity<ErrosResponse> handleResponseEntity(ImprocessavelException up, HttpServletRequest request){		
-		return response(up.getMessage(), request, HttpStatus.INTERNAL_SERVER_ERROR, LocalDateTime.now());		
+		return response(up.getMessage(), request, HttpStatus.UNPROCESSABLE_ENTITY, LocalDateTime.now());		
 	}
 	
 	private ResponseEntity<ErrosResponse> response(final String message, final HttpServletRequest request, final  HttpStatus status, LocalDateTime data ){
